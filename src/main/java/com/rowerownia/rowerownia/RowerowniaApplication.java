db28aced9c5a88c4b98ad7f31bba89cd913eceec
Myjem <siepkawiktor240@gmail.com>
2024-11-30 20:21:00 +0100

    Initial commit

---
+++ src/main/java/com/rowerownia/rowerownia/RowerowniaApplication.java
@@ -0,0 +1,13 @@
+package com.rowerownia.rowerownia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RowerowniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RowerowniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rowerownia/rowerownia/RowerowniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rowerownia.rowerownia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RowerowniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
