9612dfd9cb5fc546c518834057dca92a8d721358
Mateusz Andrzejewski <mandrzejewski132@gmail.com>
2024-12-07 01:04:44 +0100

    new name of service to repair

---
+++ src/main/java/com/rowerownia/rowerownia/entity/Repair.java
@@ -0,0 +1,73 @@
+package com.rowerownia.rowerownia.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Repair {
+    @Id
+    @SequenceGenerator(
+            name = "repair_sequence",
+            sequenceName = "repair_sequence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "repair_sequence")
+    private Integer repairId;
+    private String repairName;
+    private Integer repairTime;
+    private Integer repairPrice;
+
+    public Repair() {
+    }
+
+    public Repair(String repairName, Integer repairTime, Integer repairPrice) {
+        this.repairName = repairName;
+        this.repairTime = repairTime;
+        this.repairPrice = repairPrice;
+    }
+
+    public String getRepairName() {
+        return repairName;
+    }
+
+    public void setRepairName(String repairName) {
+        this.repairName = repairName;
+    }
+
+    public Integer getRepairTime() {
+        return repairTime;
+    }
+
+    public void setRepairTime(Integer repairTime) {
+        this.repairTime = repairTime;
+    }
+
+    public Integer getRepairPrice() {
+        return repairPrice;
+    }
+
+    public void setRepairPrice(Integer repairPrice) {
+        this.repairPrice = repairPrice;
+    }
+
+    public Integer getRepairId() {
+        return repairId;
+    }
+
+    @Override
+    public String toString() {
+        return "repair{" +
+                "repairId=" + repairId +
+                ", repairName='" + repairName + '\'' +
+                ", repairTime=" + repairTime +
+                ", repairPrice=" + repairPrice +
+                '}';
+    }
+}
---
+++ src/main/java/com/rowerownia/rowerownia/entity/RepairBooking.java
@@ -0,0 +1,85 @@
+package com.rowerownia.rowerownia.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table
+public class RepairBooking {
+    @Id
+    @SequenceGenerator(
+            name = "repairBooking_sequence",
+            sequenceName = "repairBooking_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "repairBooking_sequence")
+    private Integer repairBookingId;
+    private Integer userId;
+    private LocalDate rbookingDate;
+    private LocalDate repairDate;
+    private Enums.status repairStatus;
+
+    public RepairBooking() {
+    }
+
+    public RepairBooking(Integer userId, LocalDate rbookingDate, LocalDate repairDate, Enums.status repairStatus) {
+        this.userId = userId;
+        this.rbookingDate = rbookingDate;
+        this.repairDate = repairDate;
+        this.repairStatus = repairStatus;
+    }
+
+    public Integer getRepairBookingId() {
+        return repairBookingId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public LocalDate getRbookingDate() {
+        return rbookingDate;
+    }
+
+    public void setRbookingDate(LocalDate rbookingDate) {
+        this.rbookingDate = rbookingDate;
+    }
+
+    public LocalDate getRepairDate() {
+        return repairDate;
+    }
+
+    public void setRepairDate(LocalDate repairDate) {
+        this.repairDate = repairDate;
+    }
+
+    public Enums.status getRepairStatus() {
+        return repairStatus;
+    }
+
+    public void setRepairStatus(Enums.status repairStatus) {
+        this.repairStatus = repairStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "repairBooking{" +
+                "repairBookingId=" + repairBookingId +
+                ", userId=" + userId +
+                ", rbookingDate=" + rbookingDate +
+                ", repairDate=" + repairDate +
+                ", repairStatus=" + repairStatus +
+                '}';
+    }
+}
